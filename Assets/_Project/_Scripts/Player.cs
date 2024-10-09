945147c0e77ccfc93d56431494715e37b2c23eaf
koenfrontatie <koenvanderwaal@hotmail.com>
2024-10-09 18:26:49 +0200

    refactored player input code

--- Assets/_Project/_Scripts/Player.cs
+++ Assets/_Project/_Scripts/Player.cs
@@ -1,51 +1,59 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player: MonoBehaviour
 {
-    [SerializeField] private bool _serialInput;
+    [SerializeField] private InputType _inputType;
+    [SerializeField] PlayerIndex _playerIndex;
+    [SerializeField] private KeyCode _upKey, _downKey;
+    [SerializeField] private float _speed = 9f;
+    private static readonly float[] _HEIGHTRANGE = { -5, 5 };
+    private SerialReader _serialReader;
+    private float[] _moveInput = new float[2];
 
-    [SerializeField]
-    private KeyCode _upKey, _downKey;
-    [SerializeField]
-    private float _speed = 2f;
-
-    private float[] _heightRange;
-    private float _heightOffset;
-
-    void Start()
+    private void Start()
     {
-        _heightRange = new float[2];
-        _heightRange[0] = 5;
-        _heightRange[1] = -5;
+        _serialReader = FindObjectOfType<SerialReader>();
     }
 
     void Update()
     {
-
-        if (_serialInput) return;
-
-        if (Input.GetKey(_upKey))
+        switch (_inputType)
         {
-            MoveUp();
+            case InputType.Keyboard:
+                _moveInput[0] = Input.GetKey(_upKey) ? 1 : 0;
+                _moveInput[1] = Input.GetKey(_downKey) ? 1 : 0;
+                break;
+
+            case InputType.Balance:
+                if (_serialReader == null) break;
+
+                if (_playerIndex == PlayerIndex.Player1)
+                {
+                    _moveInput[0] = _serialReader.InputValues[0];
+                    _moveInput[1] = _serialReader.InputValues[1];
+                }
+                else
+                {
+                    _moveInput[0] = _serialReader.InputValues[2];
+                    _moveInput[1] = _serialReader.InputValues[3];
+                }
+                break;
         }
 
-        if (Input.GetKey(_downKey))
-        {
-            MoveDown();
-        }
+        float balance = (_moveInput[0] - _moveInput[1]) * Time.deltaTime * _speed;
+        float newY = Mathf.Clamp(transform.position.y + balance, _HEIGHTRANGE[0], _HEIGHTRANGE[1]);
+        transform.position = new Vector3(transform.position.x, newY);
     }
+}
 
-    public void MoveUp()
-    {
-        float newY = transform.position.y;
-        newY = Mathf.Clamp(newY + _speed * Time.deltaTime, _heightRange[1], _heightRange[0]);
-        transform.position = new Vector2(transform.position.x, newY);
-    }
+public enum InputType
+{
+    Keyboard,
+    Balance
+}
 
-    public void MoveDown()
-    {
-        float newY = transform.position.y;
-        newY = Mathf.Clamp(newY - _speed * Time.deltaTime, _heightRange[1], _heightRange[0]);
-        transform.position = new Vector2(transform.position.x, newY);
-    }
+public enum PlayerIndex
+{
+    Player1,
+    Player2
 }
--- Assets/_Project/_Scripts/SerialReader.cs
+++ Assets/_Project/_Scripts/SerialReader.cs
@@ -1,187 +1,19 @@
-//using System;
-//using System.Net;
-//using UnityEngine;
-
-//public class SerialReader : MonoBehaviour
-//{
-//    private UnitySerialPort _serialPort;
-
-//    // Booleans representing serial inputs
-//    public bool one, two, three, four;
-
-//    public Player Player1, Player2;
-
-//    void Start()
-//    {
-//        if (transform.TryGetComponent(out _serialPort))
-//        {
-//            UnitySerialPort.SerialDataParseEvent += OnSerialDataParse;
-//        }
-//    }
-
-//    void Update()
-//    {
-//        if(one)
-//        {
-//            Player1.MoveUp();
-
-//            if (GameManager.Instance.State != GameState.Playing) GameManager.Instance.UpdateGameState(GetNextEnumValue(GameManager.Instance.State));
-//        }
-
-//        if(two)
-//        {
-//            Player1.MoveDown();
-
-//            if (GameManager.Instance.State != GameState.Playing) GameManager.Instance.UpdateGameState(GetNextEnumValue(GameManager.Instance.State));
-//        }
-
-//        if(three)
-//        {
-//            Player2.MoveUp();
-
-//            if (GameManager.Instance.State != GameState.Playing) GameManager.Instance.UpdateGameState(GetNextEnumValue(GameManager.Instance.State));
-//        }
-
-//        if(four)
-//        {
-//            Player2.MoveDown();
-
-//            if (GameManager.Instance.State != GameState.Playing) GameManager.Instance.UpdateGameState(GetNextEnumValue(GameManager.Instance.State));
-//        }
-//    }
-
-//    void OnDestroy()
-//    {
-//        if (_serialPort != null)
-//        {
-//            UnitySerialPort.SerialDataParseEvent -= OnSerialDataParse;
-//        }
-//    }
-
-//    void OnSerialDataParse(string[] data, string rawData)
-//    {
-//        // Check if data contains at least one element to avoid out of bounds errors
-//        if (data == null || data.Length == 0) return;
-
-//        // Trim the data to remove any unwanted whitespace or newline characters
-//        string pData = data[0].Trim();
-
-//        // Process the parsed serial data
-
-//        switch (pData)
-//        {
-//            case "1":
-//                one = true;
-//                break;
-//            case "2":
-//                two = true;
-//                break;
-//            case "3":
-//                three = true;
-//                break;
-//            case "4":
-//                four = true;
-//                break;
-//            case "x1":
-//                one = false;
-//                break;
-//            case "x2":
-//                two = false;
-//                break;
-//            case "x3":
-//                three = false;
-//                break;
-//            case "x4":
-//                four = false;
-//                break;
-//            default:
-//                Debug.LogWarning($"Unexpected data received: {pData}");
-//                break;
-//        }
-//    }
-
-//    public static Enum GetNextEnumValue(Enum currentValue)
-//    {
-//        // Get the values of the enum
-//        MyEnum[] enumValues = (MyEnum[])Enum.GetValues(typeof(MyEnum));
-
-//        // Get the index of the current value
-//        int currentIndex = Array.IndexOf(enumValues, currentValue);
-
-//        // Calculate the next index using modulo
-//        int nextIndex = (currentIndex + 1) % enumValues.Length;
-
-//        // Return the next enum value
-//        return enumValues[nextIndex];
-//    }
-
-//}
-
 using System;
-//using System.Diagnostics;
 using UnityEngine;
+using KVDW.Extensions;
 
 public class SerialReader : MonoBehaviour
 {
+    public float[] InputValues = new float[4];
     private UnitySerialPort _serialPort;
-
-    // Booleans representing serial inputs
-    public bool one, two, three, four;
-
-    public Player Player1, Player2;
-
-    float menuTimer;
+    private float[] _serialInputValues = new float[4];
+    private float[] _serialInputCeilings = new float[] { 1f, 1f, 1f, 1f };
 
     void Start()
     {
         if (transform.TryGetComponent(out _serialPort))
         {
             UnitySerialPort.SerialDataParseEvent += OnSerialDataParse;
-        }
-    }
-
-    void Update()
-    {
-        if (one)
-        {
-            Player1.MoveUp();
-
-            MenuBehaviour();
-        }
-
-        if (two)
-        {
-            Player1.MoveDown();
-
-            MenuBehaviour();
-        }
-
-        if (three)
-        {
-            Player2.MoveUp();
-
-            MenuBehaviour();
-        }
-
-        if (four)
-        {
-            Player2.MoveDown();
-
-            MenuBehaviour();
-        }
-    }
-
-    void MenuBehaviour()
-    {
-        if (GameManager.Instance.State != GameState.Playing && Time.time > menuTimer + 1f)
-        {
-            // Get the next enum value
-            GameManager.Instance.UpdateGameState(GetNextEnumValue(GameManager.Instance.State));
-        }
-
-        if (GameManager.Instance.State == GameState.Playing)
-        {
-            menuTimer = Time.time;
         }
     }
 
@@ -199,51 +31,14 @@ public class SerialReader : MonoBehaviour
 
         string pData = data[0].Trim();
 
-        switch (pData)
+        float[] parsedValues = Array.ConvertAll(pData.Split('.'), float.Parse);
+
+        for (int i = 0; i < parsedValues.Length && i < InputValues.Length; i++)
         {
-            case "1":
-                one = true;
-                break;
-            case "2":
-                two = true;
-                break;
-            case "3":
-                three = true;
-                break;
-            case "4":
-                four = true;
-                break;
-            case "x1":
-                one = false;
-                break;
-            case "x2":
-                two = false;
-                break;
-            case "x3":
-                three = false;
-                break;
-            case "x4":
-                four = false;
-                break;
-            default:
-                Debug.LogWarning($"Unexpected data received: {pData}");
-                break;
+            _serialInputCeilings[i] = parsedValues[i] > _serialInputCeilings[i] ? parsedValues[i] : _serialInputCeilings[i];
+
+            // this normalizes the input values to a range of 0 to 1
+            InputValues[i] = parsedValues[i].Remap(0, _serialInputCeilings[i], 0, 1);
         }
-    }
-
-    // Corrected GetNextEnumValue method for GameState
-    public static GameState GetNextEnumValue(GameState currentValue)
-    {
-        // Get the values of the GameState enum
-        GameState[] enumValues = (GameState[])Enum.GetValues(typeof(GameState));
-
-        // Get the index of the current value
-        int currentIndex = Array.IndexOf(enumValues, currentValue);
-
-        // Calculate the next index using modulo to loop back
-        int nextIndex = (currentIndex + 1) % enumValues.Length;
-
-        // Return the next GameState value
-        return enumValues[nextIndex];
     }
 }
